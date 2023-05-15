2d24b72dbc64654aed80b1a2ad01f352abe3fec2
01Dri <drikill01@gmail.com>
2023-05-14 23:01:51 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/me/dri/workshopmongo/config/TestConfig.java
+++ src/main/java/me/dri/workshopmongo/config/TestConfig.java
@@ -2,6 +2,7 @@ package me.dri.workshopmongo.config;
 
 import me.dri.workshopmongo.domain.Post;
 import me.dri.workshopmongo.domain.User;
+import me.dri.workshopmongo.dtop.AuthorDTO;
 import me.dri.workshopmongo.repository.PostRepository;
 import me.dri.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,11 +34,11 @@ public class TestConfig  implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf .parse("23/01/2003"), "Partiu viagem", "Vou viajar para SP", maria);
-        Post post2 = new Post(null, sdf .parse("23/01/2003"), "Bom dia", "Acordei feliz hoje", maria);
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf .parse("23/01/2003"), "Partiu viagem", "Vou viajar para SP",  new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf .parse("23/01/2003"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
 
