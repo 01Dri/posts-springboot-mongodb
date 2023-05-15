095c342d70de7e5fc6a898d579dc61809383c80e
01Dri <drikill01@gmail.com>
2023-05-14 23:57:41 -0300

    Consulta simples com @Query

--- src/main/java/me/dri/workshopmongo/repository/PostRepository.java
+++ src/main/java/me/dri/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package me.dri.workshopmongo.repository;
 
 import me.dri.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String msg);
 }
